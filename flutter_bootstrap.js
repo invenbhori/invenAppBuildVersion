76a771af403b42010b701ef5e30ce40a09a3955a
Mustafa <inven.bhori@gmail.com>
2025-02-01 20:53:09 +0530

    check base url function ignored

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2777406114"
+    serviceWorkerVersion: "853291601"
   }
 });
